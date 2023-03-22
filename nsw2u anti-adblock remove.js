209881525b4173e1ca74c0a7a041b09a6e17c6af
SerenoV7 <82668939+SerenoV7@users.noreply.github.com>
2023-03-22 13:49:21 +0100

    Create nsw2u anti-adblock remove.js

---
+++ nsw2u anti-adblock remove.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         nsw2u.com Anti-AdBlock Remover
+// @namespace    https://sereno.pages.dev/
+// @version      1.0
+// @description  Prevent the Anti-AdBlock from EXISTING
+// @author       Sereno
+// @match        https://nsw2u.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=codepen.io
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const h4Element = document.querySelector('h4.adblock_title'); //Selects the "AdBlock Detected" text
+    const topDiv = h4Element.parentNode.parentNode.parentNode.parentNode.parentNode; //Selects the top most div that composes the block
+    topDiv.parentNode.removeChild(topDiv); //Deletes the block
+
+})();
